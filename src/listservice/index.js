30864b527551b6907f7b1cdfab549c8eda28d2d6
prototypeftt <prototypeftt@proton.me>
2023-04-12 15:24:39 +0100

    Wired to Spanner and NewList via GRPC inserting

--- src/listservice/index.js
+++ src/listservice/index.js
@@ -1,3 +1,19 @@
+// Imports the Google Cloud client library
+const { Spanner } = require('@google-cloud/spanner');
+
+const projectId = 'listly-f1ab7';
+const instanceId = 'list-instance';
+const databaseId = 'list-database';
+
+// Creates a client
+const spanner = new Spanner({
+  projectId: projectId,
+});
+
+// Gets a reference to a Cloud Spanner instance and database
+const instance = spanner.instance(instanceId);
+const database = instance.database(databaseId);
+
 const PROTO_PATH = __dirname + '/protos/list.proto';
 
 const grpc = require('@grpc/grpc-js');
@@ -26,12 +42,45 @@ const list_proto = grpc.loadPackageDefinition(packageDefinition).list;
 */
 
 function NewList(call, callback) {
-  callback(null, {  "listId": 1});
+  callback(null, { "listId": 1 });
   console.log("NewList: " + call.request.listName);
+
+  database.runTransaction(async (err, transaction) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    try {
+      const [rowCount] = await transaction.runUpdate({
+        //sql: 'INSERT INTO list (listId, userId, listName) VALUES (7,666,@listName)',
+        sql: 'INSERT Into list (listId, userId, listName) VALUES ($1,$2,$3)',
+        params: {
+          p1: call.request.listName,
+          p2: call.request.listName,
+          p3: call.request.listName,
+        },
+      });
+  
+      console.log(
+        `Successfully inserted ${rowCount} record into the list table.`
+      );
+  
+      await transaction.commit();
+    } catch (err) {
+      console.error('ERROR:', err);
+    } finally {
+      // Close the database when finished.
+      //database.close();
+    }
+  });
+
+
 }
 
 
 function main() {
+
+
   const server = new grpc.Server();
 
   server.addService(list_proto.ListService.service, { NewList: NewList });
@@ -47,6 +96,8 @@ function main() {
       server.start();
     }
   );
+
+
 
 }
 
