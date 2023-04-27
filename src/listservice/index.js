574e0c9ae54fc5a1ec56e9f2d70c981edfb0f5df
prototypeftt <prototypeftt@proton.me>
2023-04-27 16:46:18 +0100

    posrt change

--- src/listservice/index.js
+++ src/listservice/index.js
@@ -257,7 +257,6 @@ function main() {
     GetLists: GetLists
   });
 
-  console.log("Server listening on address :" + process.env.PORT);
   console.log("Server listening on port :" + config.PORT);
 
   server.bindAsync(
