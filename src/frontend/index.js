e8773c96d199b6b033561fed59b11fc2fd8386d5
prototypeftt <prototypeftt@proton.me>
2023-04-28 11:19:07 +0100

    changed frontend service address

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -34,11 +34,11 @@ service_address = config.SERVICE_ADDRESS+':'+config.SERVICE_ADDRESS_PORT;
 
 console.log('service address: '+service_address)
 
-/*const listStub = new list_proto.ListService(service_address,
-  grpc.credentials.createInsecure());*/
-
-const listStub = new list_proto.ListService('listservice-3pziucpdaa-ey.a.run.app:443',
+const listStub = new list_proto.ListService(service_address,
   grpc.credentials.createInsecure());
+
+/*const listStub = new list_proto.ListService('listservice-3pziucpdaa-ey.a.run.app:443',
+  grpc.credentials.createInsecure());*/
 
 const app = express();
 
